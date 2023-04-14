435865e3ad3fc54384be24d0a4d760cb5f8b06a9
Tom <twu@lcs.on.ca>
2023-04-14 14:50:05 -0400

    add a network service helper to load a joke from an endpoint, and displayed it in the user interface.

--- Random Jokes/Views/JokeView.swift
+++ Random Jokes/Views/JokeView.swift
@@ -11,32 +11,41 @@ struct JokeView: View {
     
     @State var punchlineOpacity = 0.0
     
-    @State var currentJoke = exampleJoke
+    @State var currentJoke: Joke?
     
     var body: some View {
         NavigationView{
             VStack{
-                Text(currentJoke.setup)
-                    .font(.title)
-                    .multilineTextAlignment(.center)
                 
-                Button(action: {
-                    withAnimation(.easeIn(duration: 1.0)){
-                        punchlineOpacity = 1.0
-                    }
-                }, label: {Image(systemName: "arrow.down.circle.fill")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 40)
-                        .tint(.black)
-                })
                 
-                Text(currentJoke.punchline)
-                    .font(.title)
-                    .multilineTextAlignment(.center)
-                    .opacity(punchlineOpacity)
+                if let currentJoke = currentJoke{
+                    Text(currentJoke.setup)
+                        .font(.title)
+                        .multilineTextAlignment(.center)
+                    
+                    Button(action: {
+                        withAnimation(.easeIn(duration: 1.0)){
+                            punchlineOpacity = 1.0
+                        }
+                    }, label: {Image(systemName: "arrow.down.circle.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 40)
+                            .tint(.black)
+                    })
+                    
+                    Text(currentJoke.punchline)
+                        .font(.title)
+                        .multilineTextAlignment(.center)
+                        .opacity(punchlineOpacity)
+                }else{
+                    ProgressView()
+                }
             }
             .navigationTitle("Random Jokes")
+        }
+        .task {
+            currentJoke = await NetworkService.fetch()
         }
     }
 }
