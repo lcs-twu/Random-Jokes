02082459d9a5cb0828e79b452e1ea6d0647fb059
Tom <twu@lcs.on.ca>
2023-04-14 14:24:38 -0400

    re-organized to add view group

---
+++ Random Jokes/Views/JokeView.swift
@@ -0,0 +1,20 @@
+//
+//  JokeView.swift
+//  Random Jokes
+//
+//  Created by Tom Wu on 2023-04-14.
+//
+
+import SwiftUI
+
+struct JokeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct JokeView_Previews: PreviewProvider {
+    static var previews: some View {
+        JokeView()
+    }
+}
