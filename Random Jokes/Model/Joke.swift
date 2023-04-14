2230d07c0f255c2433864431633f8c078a9b5dd6
Tom <twu@lcs.on.ca>
2023-04-14 14:38:31 -0400

    added a model

---
+++ Random Jokes/Model/Joke.swift
@@ -0,0 +1,17 @@
+//
+//  Joke.swift
+//  Random Jokes
+//
+//  Created by Tom Wu on 2023-04-14.
+//
+
+import Foundation
+
+struct Joke: Identifiable{
+    let type: String
+    let setup: String
+    let punchline: String
+    let id: Int
+}
+
+let exampleJoke = Joke(type: "general", setup: "How much does a hipster weigh?", punchline: "An instagram", id: 146)
