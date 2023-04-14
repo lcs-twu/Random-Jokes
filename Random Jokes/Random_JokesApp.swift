1c2aab3908a8689c3a7295088c6d91e0a8932b4b
Tom <twu@lcs.on.ca>
2023-04-14 14:19:32 -0400

    Initial Commit

---
+++ Random Jokes/Random_JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Random_JokesApp.swift
+//  Random Jokes
+//
+//  Created by Tom Wu on 2023-04-14.
+//
+
+import SwiftUI
+
+@main
+struct Random_JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
