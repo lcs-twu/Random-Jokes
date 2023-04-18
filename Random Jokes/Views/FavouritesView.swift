15b8b605a53cd1709f44b558948ee4e91d9fd7c9
Tom <twu@lcs.on.ca>
2023-04-18 09:17:02 -0400

    Added a title to the Favourites view.

--- Random Jokes/Views/FavouritesView.swift
+++ Random Jokes/Views/FavouritesView.swift
@@ -14,12 +14,15 @@ struct FavouritesView: View {
     }) var favouriteJokes
     
     var body: some View {
-        List(favouriteJokes.results){ currentJoke in
-            VStack(alignment: .leading){
-                Text(currentJoke.setup)
-                    .bold()
-                Text(currentJoke.punchline)
+        NavigationView{
+            List(favouriteJokes.results){ currentJoke in
+                VStack(alignment: .leading){
+                    Text(currentJoke.setup)
+                        .bold()
+                    Text(currentJoke.punchline)
+                }
             }
+            .navigationTitle("Favourites")
         }
     }
 }
