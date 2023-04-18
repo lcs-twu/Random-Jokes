f406deb2c23ac0a58657a7ebea4f55360e55963f
Tom <twu@lcs.on.ca>
2023-04-18 09:07:00 -0400

    Started to add view to see the list of saved / favourite jokes.

---
+++ Random Jokes/Views/FavouritesView.swift
@@ -0,0 +1,20 @@
+//
+//  FavouritesView.swift
+//  Random Jokes
+//
+//  Created by Tom Wu on 2023-04-18.
+//
+
+import SwiftUI
+
+struct FavouritesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FavouritesView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavouritesView()
+    }
+}
